4bac5158f3b146756be63cab3474b818146d4a94
Mohammed Malik <malikmm@hotmail.com>
2022-05-11 17:32:35 +0100

    new content

---
+++ src/com/malik/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.malik.helloworld;
+
+/**
+ * @author malikmo
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
